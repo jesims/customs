b0ecddfd0ec27401b18b8b6645a97c6274918602
André de Jager <andrethehunter@gmail.com>
2020-01-14 14:10:00 +1000

    Added no arg overloads to leiningen functions

--- src/io/jesi/customs/leiningen.clj
+++ src/io/jesi/customs/leiningen.clj
@@ -4,7 +4,7 @@
     [clojure.tools.namespace.find :as ns-find]
     [com.rpl.specter :as sp]
     [io.jesi.backpack :as bp]
-    [io.jesi.customs.strict :refer [is is=]]
+    [io.jesi.customs.strict :refer :all]
     [leiningen.core.main]
     [leiningen.core.project :as project]
     [leiningen.jar])
@@ -13,6 +13,7 @@
     (java.util.zip ZipInputStream)))
 
 (defn read-project
+  ([] (read-project "project.clj"))
   ([file] (read-project file [:default]))
   ([file profiles]
    (project/ensure-dynamic-classloader)
@@ -20,38 +21,42 @@
 
 (defn find-gen-class-ns
   "Returns a map of ns symbols and it's :gen-class definition (as a map)"
-  [project]
-  (->> (for [dir (:source-paths project)]
-         (->> dir
-              io/file
-              ns-find/find-ns-decls-in-dir
-              (map (fn [ns-decl]
-                     (when-let [gen-class (sp/select-one [sp/ALL (bp/and-fn seqable? (bp/compr first (bp/p= :gen-class)))] ns-decl)]
-                       [(second ns-decl)
-                        (->> gen-class
-                             rest
-                             (apply hash-map))])))
-              (into {})))
-       (apply merge)))
+  ([] (find-gen-class-ns (read-project)))
+  ([project]
+   (->> (for [dir (:source-paths project)]
+          (->> dir
+               io/file
+               ns-find/find-ns-decls-in-dir
+               (map (fn [ns-decl]
+                      (when-let [gen-class (sp/select-one [sp/ALL (bp/and-fn seqable? (bp/compr first (bp/p= :gen-class)))] ns-decl)]
+                        [(second ns-decl)
+                         (->> gen-class
+                              rest
+                              (apply hash-map))])))
+               (into {})))
+        (apply merge))))
 
-(defn is-gen-class-in-aot [project]
-  (let [aot (:aot project)]
-    (when (is (seq aot))
-      (is= aot
-           (keys (find-gen-class-ns project))))))
+(defn is-gen-class-in-aot
+  ([] (is-gen-class-in-aot (read-project)))
+  ([project]
+   (let [aot (:aot project)]
+     (when (is (seq aot))
+       (is= aot
+            (keys (find-gen-class-ns project)))))))
 
 (defn build-jar
   "Runs the `jar` command, returning the path of the built jar"
-  [project]
-  (let [out (let [s (new StringWriter)]
-              (binding [*out* s
-                        *err* s
-                        leiningen.core.main/*info* true
-                        leiningen.core.main/*exit-process?* false]
-                (leiningen.jar/jar project))
-              (-> s str .trim))]
-    (println out)
-    (some->> out (re-find #"(?<=Created ).+") str .trim)))
+  ([] (build-jar (read-project)))
+  ([project]
+   (let [out (let [s (new StringWriter)]
+               (binding [*out* s
+                         *err* s
+                         leiningen.core.main/*info* true
+                         leiningen.core.main/*exit-process?* false]
+                 (leiningen.jar/jar project))
+               (-> s str .trim))]
+     (println out)
+     (some->> out (re-find #"(?<=Created ).+") str .trim))))
 
 ;TODO move to backpack?
 (defn- list-zip [file]
@@ -61,5 +66,8 @@
         (recur (conj! paths (-> entry .getName)))
         (persistent! paths)))))
 
-(defn list-jar [jar-path]
-  (some-> jar-path list-zip seq))
+(defn list-jar
+  ([]
+   (-> (read-project) build-jar list-jar))
+  ([jar-path]
+   (some-> jar-path list-zip seq)))
